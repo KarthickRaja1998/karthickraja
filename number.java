2b8deb38e5fb0f3c8e031774cc7f60733116909a
KarthickRaja1998 <48713441+KarthickRaja1998@users.noreply.github.com>
2019-07-02 15:04:18 +0530

    Create number.java

---
+++ number.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class number 
+{
+public static void main(Strings args[])
+{
+Scanner s=new Scanner(System.in);1
+String n1=s.nextLine();
+String n2=s.nextLine();
+System.out.print(n1+n2);
+}
+}
