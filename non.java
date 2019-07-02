46cd45f7ca4df2da1763c60087d4cc264f732d0d
KarthickRaja1998 <48713441+KarthickRaja1998@users.noreply.github.com>
2019-07-02 11:21:46 +0530

    Create non.java

---
+++ non.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class non
+{
+public static  void main(String arg[])
+{
+int n=new Scanner(System.in).nextInt();
+if(n>0)
+System.out.print("Positive");
+else if(n<0)
+System.out.print("Negative");
+else
+System.out.print("Zero");
+}
+}
